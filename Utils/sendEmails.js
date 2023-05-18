e341689796d3f43a1d3c3a43c71446edc5d75ea0
Zacharia <ndunguzachary24@gmail.com>
2023-05-18 23:17:01 +0300

    Added Utils folder for the sendEmail file, the file contain nodemailer function that sends mails

---
+++ Utils/sendEmails.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+
+const myEmail = process.env.USER_GMAIL;
+const myPass = process.env.USER_PASSWORD;
+const myHost = process.env.EMAIL_HOST
+
+const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
+  const transporter = nodemailer.createTransport({
+    host: myHost,
+    port: "587",
+    auth: {
+      user: myEmail,
+      pass: myPass,
+    },
+  });
+
+  const emailOptions = {
+    from: sent_from ,
+    to: send_to,
+    replyTo: reply_to,
+    subject: subject,
+    html: message,
+  };
+
+  // Send Email
+  transporter.sendMail(emailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = sendEmail;
--- index.js
+++ index.js
@@ -1,40 +1,76 @@
-import express from "express";
-import cors from "cors";
-import nodemailer from "nodemailer";
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = 5000;
+const myemail = process.env.USER_GMAIL;
+const mypass = process.env.USER_PASSWORD;
 
-const PORT = process.env.PORT || 5500;
 
-const  myemail = process.env.USER_GMAIL
-const mypass = process.env.USER_PASSWORD
-
+//  middleware 
 const app = express();
-
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.post("/post", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
 });
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: myemail,
-    pass: mypass,
-  },
+// routes 
+app.get("/", (req, res) => {
+  sendEmail()
+    .then((response) => res.send(response.message))
+    .catch((error) => res.status(500).send(error.message));
 });
-const mailOptions = {
-  from: req.body.email,
-  to: "vincvneture@gmail.com",
-  text: `From: ${req.body.emaill}\n\n${req.body.message}`,
-};
+    const transporter = nodemailer.createTransport({
+     service: "gmail",
+      auth: {
+        user: myemail,
+        pass: mypass,
+      },
+    });
+    const mail_configs = {
+      from: "myemail",
+      to: "ndunguzachary24@gmail.com",
+      subject: "New Order",
+      text: values
+//       html: `< !DOCTYPE html>
+//       <html lang="en" >
+// <head>
+//   <meta charset="UTF-8">
+//   <title>CodePen - OTP Email Template</title>
+// </head>
+// <body>
+// <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+// <div style="margin:50px auto;width:70%;padding:20px 0">
+//   <div style="border-bottom:1px solid #eee">
+//     <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">New Order</a>
+//   </div>
+//   <p>Customer Name : ${values}</p>
 
-app.listen(PORT, (res, req) => {
-  if (error) {
-    console.log("Servr failed");
-  } else {
-    console.log(`Listening at port ${PORT}`);
-  }
+//   <hr style="border:none;border-top:1px solid #eee" />
+//   <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+//   </div>
+// </div>
+// </div> 
+// </body>
+// </html>`,
+    };
+
+app.get("/", (req, res) => {
+  sendEmail()
+    .then((response) => res.send(response.message))
+    .catch((error) => res.status(500).send(error.message));
+});
+
+app.post("/send_email", function (req, res){
+  sendEmail(req.body)
+    .then((response) => res.send(response.message))
+
+    .catch((error) => res.status(500).send(error.message));
+});
+
+app.listen(port, () => {
+  console.log("Server up and running");
 });
